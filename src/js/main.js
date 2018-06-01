388b14e455180e17dba810b4279e3225e1855d6a
lisimin02 <lismin02@58ganji.com>
2018-06-01 18:01:33 +0800

    添加简单的测试页面

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lisimin02 on 2018/6/1.
+ */
+function changeColor() {
+  window.document.getElementsByTagName("body")[0].style.background = "#F00";
+}
+window.document.onclick = changeColor;
